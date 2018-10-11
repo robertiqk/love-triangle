9330bfb2c3bce31f8082555d85b0eacf3966da9b
robertiqk <delux1945@gmail.com>
2018-10-11 22:56:57 +0300

    refactor: code rewrite

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-    let k;
-    let k_2;
-    let k_3;
     let countTriangles = 0;
-    const trianglesArr = [];
-    for(let n = 0; n < preferences.length; n++) {
-      k = preferences[n];
-      k_2 = preferences[k - 1];
-      k_3 = preferences[k_2 - 1];
-      if(n + 1 === k_3 && !(~trianglesArr.indexOf(k)) && !(~trianglesArr.indexOf(k_2)) && !(~trianglesArr.indexOf(k_3)) && k !== k_2 && k !== k_3 && k_2 !== k_3 ) {
-        trianglesArr.push(k, k_2, k_3);
+   
+    for(let i = 0, leng = preferences.length; i < leng; i++) {
+      const firstAngle = preferences[i];
+      const secAngle = preferences[firstAngle - 1];
+      const thirdAngle = preferences[secAngle - 1];
+
+      const firstEqualsThird = i + 1 === thirdAngle;
+      const notNarciss = i + 1 !== preferences[i];
+
+      if(firstEqualsThird && notNarciss) {
         countTriangles += 1;
       }
     }
-    return countTriangles;
+
+    return countTriangles / 3;
 }
